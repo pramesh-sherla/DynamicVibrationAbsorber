5d9f8e0ea9dcfb811ff67b8c0931a3bf85c873e3
Pramesh Sherla <pramesherla121@gmail.com>
2020-12-04 16:25:39 +0530

    Taking Signal as a input. Also changed the min step size according to get a sine wave.

--- ConstantValues.m
+++ ConstantValues.m
@@ -1,5 +1,16 @@
+%% Given Values
 M1 = 100 ; %Mass of 1st object
 k1 = 50 ;  %Spring coefficient of Spring 1
 b = 50;    %Damping Coefficient of Damper 1
+
+%% input signal
+h2r = 2 * pi;
+Fs = 48000;
+tStop = 10;
+dt = 1/Fs;
+t = (0:dt:tStop)';
+sigFreq = 1;
+amp = 2;
+sig = amp* sin(sigFreq * t *h2r);
 
 
